5445680ee2455dad56d6ce59eddd8a7806cd2aba
2BWeru <wamaithaweru19@gmail.com>
2023-10-15 20:46:41 +0300

    create a home, view

--- book/views.py
+++ book/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    
+    return render(request,'home.html')
+
+
+
+def register(request):
+
+    return render(request,'signup.html')
+
+
+
+def login(request):
+
+    return render(request, 'login.html')
+
+
+
+def orders(request):
+
+    return render(request,'orders.html')
+
+
+
+def checkout(request):
+    return render(request,'checkout.html')
+
