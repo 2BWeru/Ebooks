5fa97e80cf725769a8864dd80a6430e648762509
2BWeru <wamaithaweru19@gmail.com>
2023-10-20 21:56:57 +0300

    add cart,products,authentication app to installed apps

--- EBooks/settings.py
+++ EBooks/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book'
+    'book',
+    'authentication',
+    'cart',
+    'products',
 ]
 
 MIDDLEWARE = [
