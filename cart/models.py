0ce54954642628af6e04d984ddb03be200489eeb
2BWeru <wamaithaweru19@gmail.com>
2023-11-02 20:07:24 +0300

    create cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    name = models.CharField(max_length=60) 
+    price = models.IntegerField(default=0) 
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
+    description = models.CharField( 
+        max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/') 
