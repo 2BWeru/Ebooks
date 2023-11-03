e82e7d20fc445a9f86cf32f9989606f25dca41a1
2BWeru <wamaithaweru19@gmail.com>
2023-11-03 20:16:58 +0300

    create order model

--- cart/models.py
+++ cart/models.py
@@ -8,3 +8,15 @@ class Cart(models.Model):
     description = models.CharField( 
         max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/products/') 
+
+class Order(models.Model):
+    product = models.ForeignKey(Products, 
+                                on_delete=models.CASCADE) 
+    customer = models.ForeignKey(Customer, 
+                                 on_delete=models.CASCADE) 
+    quantity = models.IntegerField(default=1) 
+    price = models.IntegerField() 
+    address = models.CharField(max_length=50, default='', blank=True) 
+    phone = models.CharField(max_length=50, default='', blank=True) 
+    date = models.DateField(default=datetime.datetime.today) 
+    status = models.BooleanField(default=False) 
